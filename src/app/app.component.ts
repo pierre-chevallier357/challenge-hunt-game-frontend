441357acfc5713c907e46378e9469baff155a158
l3miage-laurass <lauras.samuel@gmail.com>
2021-04-13 17:58:29 +0200

    V0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
 
 import firebase from 'firebase/app';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+export interface User {
+  login: string,
+  age : number
+}
+
+export interface Defi {
+  datedecreation: string
+  descritption: string
+  id: string
+  titre: string
+}
 
 @Component({
   selector: 'app-root',
@@ -10,12 +23,19 @@ import firebase from 'firebase/app';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
 
-  constructor(public auth: AngularFireAuth) {}
+  users : User[] = [];
+  defis : Defi[] = [];
+
+  constructor(public auth: AngularFireAuth) {
+    this.getUsers();
+    this.getDefis();
+  }
 
   login(): void {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -27,5 +47,17 @@ export class AppComponent {
 
   logout(): void {
     this.auth.signOut();
+  }
+
+  getUsers(): void{
+    fetch('https://ttg-xi.herokuapp.com/api/users/')
+    .then(response => response.json())
+    .then(users => this.users = users);
+  }
+
+  getDefis(): void{
+    fetch('https://ttg-xi.herokuapp.com/api/defis/')
+    .then(response => response.json())
+    .then(defis => this.defis = defis);
   }
 }
