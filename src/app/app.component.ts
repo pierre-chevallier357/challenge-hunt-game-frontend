bfd372cb8f979e3d14954217812982e89d3952e1
l3miage-laurass <lauras.samuel@gmail.com>
2021-04-14 11:42:24 +0200

    V0.4
    ajout de la gestion d'erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,12 @@ import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
 
 import firebase from 'firebase/app';
 
-export interface User {
+export interface ErrorManager {
+  ChamisTableErrror : boolean,
+  DefisTableErrror : boolean,
+}
+
+export interface Chamis {
   login: string,
   age : number
 }
@@ -28,12 +33,20 @@ export class AppComponent {
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
 
-  users : User[] = [];
+  //un chamis est un user qui a crée des defis
+  chamis : Chamis[] = [];
   defis : Defi[] = [];
+
+  errorManager: ErrorManager = ({
+    ChamisTableErrror : false,
+    DefisTableErrror : false
+  });
+
+  //debug, en attendant
   boutonLogs = false;
 
   constructor(public auth: AngularFireAuth) {
-    this.getUsers();
+    this.getChamis();
     this.getDefis();
   }
 
@@ -49,16 +62,18 @@ export class AppComponent {
     this.auth.signOut();
   }
 
-  getUsers(): void {
-    fetch('https://ttg-xi.herokuapp.com/api/users/')
-    .then(response => response.json())
-    .then(users => this.users = users);
+  getChamis(): void {
+      fetch('https://ttg-xi.herokuapp.com/api/user/')
+      .then(response => response.json())
+      .then(chamisFromServer => {this.chamis = chamisFromServer })
+      .catch(error           => {this.errorManager.ChamisTableErrror = true  ; console.log(error)  });
   }
 
   getDefis(): void {
     fetch('https://ttg-xi.herokuapp.com/api/defis/')
     .then(response => response.json())
-    .then(defis => this.defis = defis);
+    .then(defisFromServer => {this.defis = defisFromServer })
+    .catch(error          => {this.errorManager.DefisTableErrror = true  ; console.log(error) });
   }
 
   afficherLogs(): void {
