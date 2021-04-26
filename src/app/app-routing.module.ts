591ac2f7c4c64c5171b07da9a27bbb8ec6cf1790
l3miage-laurass <lauras.samuel@gmail.com>
2021-04-26 14:55:00 +0200

    rooting simple

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { IdeaComponent } from './idea/idea.component';
+import { MyChallengesComponent } from './my-challenges/my-challenges.component';
 import { ChallengeComponent } from './challenge/challenge.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +9,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'challenge', component: ChallengeComponent },
+  { path: 'myChallenges', component: MyChallengesComponent },
+  { path: 'idea', component: IdeaComponent },
+
+
 
   //sinon rediriger vers
   {path: '**', redirectTo: ''}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,11 @@ export interface Defi {
   description: string,
   datedecreation: string
 }
+export interface SidebarButton {
+  title : string,
+  logo: string,
+  rooterLink: string
+}
 
 @Component({
   selector: 'app-root',
@@ -32,6 +37,30 @@ export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
+
+  sidebar : SidebarButton[] = [
+    {
+      title : "Page d'accueil",
+      logo: "home",
+      rooterLink: "/"
+    },
+    {
+      title : "Chercher un défi",
+      logo: "explore",
+      rooterLink: "/challenge"
+    },
+    {
+      title : "Mes Visites & Défis",
+      logo: "contact_page",
+      rooterLink: "/myChallenges"
+    },
+    {
+      title : "Soumettre une idée aux Devs",
+      logo: "lightbulb",
+      rooterLink: "/idea"
+    }
+  ]
+
 
 
   title: any; //debug visualcode
