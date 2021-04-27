d6e01175c1460205fe69531e381490c538d089ef
Léo <contact.leeo97one@gmail.com>
2021-04-27 12:34:33 +0200

    app-root : fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,16 @@ import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
 import { Router } from '@angular/router';
 
 import firebase from 'firebase/app';
-import { Chamis } from './chamis';
 
 export interface ErrorManager {
-  ChamisTableErrror : boolean,
-  DefisTableErrror : boolean,
+  ChamisTableErrror: boolean;
+  DefisTableErrror: boolean;
 }
-export interface Defi {
-  id: string,
-  titre: string,
-  auteur: string,
-  description: string,
-  datedecreation: string
-}
+
 export interface SidebarButton {
-  title : string,
-  logo: string,
-  rooterLink: string
+  title: string;
+  logo: string;
+  rooterLink: string;
 }
 
 @Component({
@@ -31,38 +24,40 @@ export interface SidebarButton {
 })
 
 export class AppComponent {
+
+  constructor(public auth: AngularFireAuth, private router: Router) {
+  }
   dataIconGoogle = 'assets/images/iconGoogle.png';
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
 
-  sidebar : SidebarButton[] = [
+  sidebar: SidebarButton[] = [
     {
-      title : "Page d'accueil",
-      logo: "home",
-      rooterLink: "/"
+      title : 'Page d\'accueil',
+      logo: 'home',
+      rooterLink: '/'
     },
     {
-      title : "Chercher un défi",
-      logo: "explore",
-      rooterLink: "/challenge"
+      title : 'Chercher un défi',
+      logo: 'explore',
+      rooterLink: '/challenge'
     },
     {
-      title : "Mes Visites & Défis",
-      logo: "contact_page",
-      rooterLink: "/myChallenges"
+      title : 'Mes Visites & Défis',
+      logo: 'contact_page',
+      rooterLink: '/myChallenges'
     },
     {
-      title : "Soumettre une idée aux Devs",
-      logo: "lightbulb",
-      rooterLink: "/idea"
+      title : 'Soumettre une idée aux Devs',
+      logo: 'lightbulb',
+      rooterLink: '/idea'
     }
-  ]
+  ];
 
-  title: any; //debug visualcode
+  title: any; // debug visualcode
 
-  constructor(public auth: AngularFireAuth, private router: Router) {
-
-  }
+  events: string[] = [];
+  opened!: boolean;
 
   login(): void {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -70,11 +65,8 @@ export class AppComponent {
       prompt: 'select_account'
     });
     this.auth.signInWithPopup(provider);
-    //envoyer une requette HTML pour enregister l'utilisateur
+    // envoyer une requette HTML pour enregister l'utilisateur
   }
-
-  events: string[] = [];
-  opened!: boolean;
 
   logout(): void {
     this.auth.signOut();
