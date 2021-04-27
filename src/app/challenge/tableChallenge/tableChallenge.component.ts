75d83b4608e6f09eab108a34404a0b642b8cfe2d
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2021-04-27 16:18:59 +0200

    Fix tableChallenge

--- src/app/challenge/tableChallenge/tableChallenge.component.ts
+++ src/app/challenge/tableChallenge/tableChallenge.component.ts
@@ -1,39 +1,17 @@
-
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator }  from '@angular/material/paginator';
 import { Input } from '@angular/core';
+
 import { Defi } from 'src/app/defi';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-/**
- * @title Table with sorting
- */
 @Component({
   selector: 'table-challenge',
   styleUrls: ['tableChallenge.component.scss'],
   templateUrl: 'tableChallenge.component.html',
 })
+
 export class TableChallengeComponent implements AfterViewInit {
   displayedColumns: string[] = ['idDefi', 'titre', 'motsClefs', 'description','duree'];
 
@@ -52,8 +30,3 @@ export class TableChallengeComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator
   }
 }
-
-
-/**  Copyright 2020 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
