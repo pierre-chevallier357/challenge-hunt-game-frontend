32196bee88d5aa6b36d1fb74588c7c1e01ffdebe
Léo <contact.leeo97one@gmail.com>
2021-04-27 12:38:05 +0200

    challenge : fixes

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -1,26 +1,19 @@
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Chami } from '../chami';
+import { Defi } from '../defi';
 import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
 
-import { Chamis } from '../chamis';
-
 export interface ErrorManager {
-  ChamisTableErrror : boolean,
-  DefisTableErrror : boolean,
+  ChamisTableErrror: boolean;
+  DefisTableErrror: boolean;
 }
-export interface Defi {
-  id: string,
-  titre: string,
-  auteur: string,
-  description: string,
-  datedecreation: string
-}
+
 export interface SearchResultsChamis {
   total: number;
-  results: Array<Chamis>;
+  results: Array<Chami>;
 }
-
 
 @Component({
   selector: 'app-challenge',
@@ -28,12 +21,11 @@ export interface SearchResultsChamis {
   styleUrls: ['../app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChallengeComponent{
+export class ChallengeComponent {
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
   tileLayerUrl = OSM_TILE_LAYER_URL;
 
-
-  chamisObs = this.http.get<Chamis[]>('https://ttg-xi.herokuapp.com/api/chamis/');
+  chamisObs = this.http.get<Chami[]>('https://ttg-xi.herokuapp.com/api/chamis/');
   defisObs = this.http.get<Defi[]>('https://ttg-xi.herokuapp.com/api/defis/');
 
   @Input() auth!: AngularFireAuth;
@@ -45,14 +37,11 @@ export class ChallengeComponent{
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
-
-  //Debug : temporaire
+  // Debug : temporaire
   boutonLogs = false;
   title: any;
 
 
   constructor(private http: HttpClient) {
   }
-
-
 }
