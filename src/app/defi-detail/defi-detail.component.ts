d355c94faf75ea1a4fa4aead1755968f9a74c442
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-04-29 16:49:47 +0200

    affichage defis + visite + srrvice maj

--- src/app/defi-detail/defi-detail.component.ts
+++ src/app/defi-detail/defi-detail.component.ts
@@ -1,3 +1,4 @@
+import { VisiteService } from './../service/visite.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -6,6 +7,7 @@ import { Chami } from '../interface/chami';
 import { DefiService } from '../service/defi.service';
 import { Defi } from '../interface/defi';
 import { Observable } from 'rxjs';
+import { Visite } from '../interface/visite';
 
 @Component({
   selector: 'app-defi-detail',
@@ -16,8 +18,9 @@ export class DefiDetailComponent implements OnInit {
 
   defi!: Defi;
   chami!: Chami;
+  visiteObs: Observable<Visite[]> = this.visiteService.getAllVisite();
 
-  constructor(private chamiService: ChamiService, private defiService: DefiService, private route: ActivatedRoute) { }
+  constructor(private chamiService: ChamiService, private defiService: DefiService, private visiteService: VisiteService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap;
@@ -25,6 +28,7 @@ export class DefiDetailComponent implements OnInit {
     this.defiService.getDefiByidDefi(idDefi).subscribe(defi => {
       this.defi = defi;
       this.chamiService.getChamiByUid(defi.uid).subscribe(chami => this.chami = chami);});
+      this.visiteObs = this.visiteService.getVisiteByIdDefi(this.defi.idDefi);
   }
 
 }
--- src/app/service/visite.service.ts
+++ src/app/service/visite.service.ts
@@ -8,17 +8,22 @@ import { Visite } from '../interface/visite';
 export class VisiteService {
 
   private visiteUrl = 'https://ttg-xi.herokuapp.com/api/visite/';
+  private defiUrl = 'https://ttg-xi.herokuapp.com/api/defis/';
 
   constructor(private http: HttpClient) {
   }
 
   getVisiteByidVisite(idVisite: number) {
     const url = `${this.visiteUrl}${idVisite}`;
-
     return this.http.get<Visite>(url);
   }
 
-  getAllArret() {
+    getVisiteByIdDefi(IdDefi: string) {
+    const url = `${this.defiUrl}${IdDefi}`+`/visite`;
+    return this.http.get<Visite[]>(url);
+  }
+
+  getAllVisite() {
     return this.http.get<Visite[]>(this.visiteUrl);
   }
 }
