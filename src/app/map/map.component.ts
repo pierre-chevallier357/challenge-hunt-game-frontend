bc1eb17378d87ce4ed742c73b8078621baa61c0b
Léo <contact.leeo97one@gmail.com>
2021-04-30 16:21:23 +0200

    carte : afficher la bonne couleur pour chaque ligne

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,13 +13,10 @@ import { MultiLineString, FeatureCollection } from 'geojson';
 export class MapComponent implements OnInit {
   tileLayerUrl = OSM_TILE_LAYER_URL;
   iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
-  geoJSON!: FeatureCollection<MultiLineString>;
-  color = '214,122,127'; // TODO: valeur constante temporaire
+  geoJSON = this.http.get<FeatureCollection<MultiLineString>>('https://data.mobilites-m.fr/api/lines/json?types=ligne&reseaux=SEM');
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<FeatureCollection<MultiLineString>>('https://data.mobilites-m.fr/api/lines/json?types=ligne&reseaux=SEM')
-      .subscribe(json => this.geoJSON = json);
   }
 }
