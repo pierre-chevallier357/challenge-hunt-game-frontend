5cad8d474bd7d33b431ee1621598c847cc6541a4
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-05-03 21:52:54 +0200

    Creator fonctionelle (faut le connecter à la BD maintenant)
    Il manque encore la liste des arrêts + calculs nombre de points + recupération uid de l'user

--- src/app/defi-creator/defi-creator.component.ts
+++ src/app/defi-creator/defi-creator.component.ts
@@ -1,4 +1,6 @@
-import { DefiType } from 'src/app/interface/defi';
+import { Indice } from './../interface/indice';
+import { Question } from './../interface/question';
+import { Defi,DefiType } from 'src/app/interface/defi';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -12,10 +14,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DefiCreatorComponent implements OnInit {
 
   defiForm!: FormGroup;
-  submitted: Boolean= true;
+  questionForm!: FormGroup
+  submitted: Boolean= false;
   DefiType = DefiType;
-  contenuePrologue!: string;
-  contenueDescription!: string;
+  listeQuestion: Partial<Question>[] = [];
+  listeIndice: Partial<Indice>[] = [];
+  leDefi!: Partial<Defi>;
+  incrementation: number=0;
+  laQuestion!: Partial<Question>;
+  lIndice!: Partial<Indice>;
+  submitedTwo: Boolean= false;
+  show!: string;
+  elem: number = 0;
+
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -29,14 +40,22 @@ export class DefiCreatorComponent implements OnInit {
       duree: ['', Validators.required],
       prologue: ['', Validators.required],
       description: ['', Validators.required],
+      epilogue: ['', Validators.required],
+      commentaire: ['', Validators.required],
+    });
+    this.questionForm = this.formBuilder.group({
+      question: ['', Validators.required],
+      pointsqss: ['', Validators.required],
+      reponse: ['', Validators.required],
+      indice: ['', Validators.required],
+      pointsi: ['', Validators.required],
     }
     )
     this.onReset();
   }
 
-  idDefi: number = Math.floor(Math.random() * (10000-100) + 100);
-
   get f() { return this.defiForm.controls; }
+  get g() { return this.questionForm.controls; }
 
   OnSubmit(){
     this.submitted = true;
@@ -44,11 +63,57 @@ export class DefiCreatorComponent implements OnInit {
       return;
     }
     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.defiForm.value, null, 4));
+    this.leDefi = {
+      titre: this.defiForm.get('titre')?.value,
+      uid: 12,                                                       // RECUPERER SUR LA PAGE
+      defiType: this.defiForm.get('type')?.value,
+      dateCreation: new Date(),
+      dateModification: new Date(),
+      versionD: 1,
+      points: 10,                                                   //A CALCULER A LA FIN
+      motsClefs: this.defiForm.get('motsClefs')?.value,
+      duree: this.defiForm.get('duree')?.value,
+      idArret: 1,                                                   //GET WITH API
+      description: this.defiForm.get('descritpion')?.value,
+      prologue: this.defiForm.get('prologue')?.value,
+      epilogue: this.defiForm.get('epilogue')?.value,
+      commentaire: this.defiForm.get('commentaire')?.value,
+    };
+    }
+
+
+  OnSubmitTwo(){
+    this.submitedTwo = true;
+    if (this.questionForm.invalid){
+      return;
+    }
+    alert('SUCCESS TWO!! :-)\n\n' + JSON.stringify(this.questionForm.value, null, 4));
+    this.incrementation += 1;
+    this.laQuestion = {
+      question: this.questionForm.get('question')?.value,
+      points: this.questionForm.get('pointsqss')?.value,
+      secret: this.questionForm.get('question')?.value,
+      numero: this.incrementation,
+    };
+
+   this.lIndice =  {
+      description: this.questionForm.get('question')?.value,
+      points: this.questionForm.get('pointsi')?.value,
+      numero: this.incrementation,
+    };
+    this.listeIndice.push(this.lIndice);
+    this.listeQuestion.push(this.laQuestion);
+    console.log(this.listeQuestion);
+    this.elem = this.listeIndice.length;
   }
 
   onReset() {
     this.submitted = false;
     this.defiForm.reset();
-}
 
+}
+  onResetTwo(){
+    this.submitedTwo = false;
+    this.questionForm.reset();
+  }
 }
