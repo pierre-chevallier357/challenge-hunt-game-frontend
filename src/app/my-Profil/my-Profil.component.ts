8fb4700d25b7f4ae2200fc9dd8af5f85d5050d5c
l3miage-laurass <lauras.samuel@gmail.com>
2021-05-04 21:01:31 +0200

    profil page

--- src/app/my-Profil/my-Profil.component.ts
+++ src/app/my-Profil/my-Profil.component.ts
@@ -1,8 +1,12 @@
+import { Profil } from './../interface/profil';
+import { HttpClient } from '@angular/common/http';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { Chami } from './../interface/chami';
 import { ChamiService } from './../service/chami.service';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-Profil',
@@ -11,12 +15,47 @@ import { ChamiService } from './../service/chami.service';
 })
 export class MyProfilComponent implements OnInit {
   profil!: Chami ;
+  profilObs$!:Observable<Chami>
 
-  constructor(private chamiService: ChamiService, private auth: AngularFireAuth) {}
+  @ViewChild('pseudo') pseudoInput!:ElementRef;
+  @ViewChild('age') ageInput!:ElementRef;
+  @ViewChild('ville') villeInput!:ElementRef;
+  @ViewChild('description') descriptionInput!:ElementRef;
+
+
+  constructor(
+    private chamiService: ChamiService,
+    private auth: AngularFireAuth,
+    private html:HttpClient) {
+      this.profilObs$ = this.auth.user.pipe(
+        switchMap((user) =>
+          this.chamiService.getChamiByUid(1001).pipe(
+            map((chami:Chami)=>{
+              this.pseudoInput.nativeElement.value = chami.pseudo;
+              this.ageInput.nativeElement.value = chami.age;
+              this.villeInput.nativeElement.value = chami.ville;
+              this.descriptionInput.nativeElement.value = chami.description;
+              return chami;
+            })
+
+          )));
+    }
 
   ngOnInit() {}
 
-  creation(chami: Chami): void {}
+  get authObs(){
+    return this.auth.user;
+  }
+
+
+  submitCreate(chami: Chami): void {
+    if (!chami.pseudo || !chami.age || !chami.ville){
+      alert("mauvais arguments");
+    } else {
+      this.profil = chami;
+      //this.html.post('https://ttg-xi.herokuapp.com/api/chami/',chami);
+    }
+  }
 
   integer(s: string): number {
     return Number(s);
