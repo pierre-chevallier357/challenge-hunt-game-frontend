49f718477439e0e01a56acbdcd0df9ebcb97cb61
l3miage-kailir <77158282+l3miage-kailir@users.noreply.github.com>
2021-05-06 16:22:03 +0200

    Revert "Ez work around"
    This reverts commit 21a88a89e74792038a666d68266433e76d88a17c.

--- src/app/challenge-accepted/challenge-accepted.component.ts
+++ src/app/challenge-accepted/challenge-accepted.component.ts
@@ -34,40 +34,24 @@ export class ChallengeAcceptedComponent implements OnInit {
         questions.map(question => this.listeReponse.push({
           question: question.idQuestion
         }
+
         ));
       }
     );
   }
 
-  onSubmit(reponseValue: string, id: number): void {
-    for(let reponse of this.listeReponse){
-      if(reponse.question === id){
-        reponse.reponse = reponseValue;
-        return;
-      }
-    }
-    this.listeReponse.push({
-      reponse: reponseValue,
-      question: id,
-    })
+  onSubmit(reponseValue: string, idQuestion: number): void {
+    const reponse = this.listeReponse.find(reponseFound => reponseFound.question = idQuestion);
+    if (reponse) { reponse.reponse = reponseValue; }
   }
 
   indiceUsed(idQuestion: number,numero: number): void {
-    for(let reponse of this.listeReponse){
-      if(reponse.question === idQuestion){
-        reponse.indiceUtilise = true;
-        alert('Vous avez utilisez un indice: \n' + this.questions[numero-1].indice);
-        return;
-      }
-    }
-
+    const reponse = this.listeReponse.find(reponseFound => reponseFound.question = idQuestion);
+    if (reponse) { reponse.indiceUtilise = true; }
+    alert('Vous avez utilisez un indice: \n' + this.questions[numero-1].indice);
   }
 
   onSubmitValidey(): void{
-    for(let reponse of this.listeReponse){
-      if(reponse.indiceUtilise != true){
-        reponse.indiceUtilise = false;
+
   }
-}
-}
 }
